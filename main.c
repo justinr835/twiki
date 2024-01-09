badabbae7efe327663eb84e40d4c6d196f77a2af
Justin R <140418304+justinroach835@users.noreply.github.com>
2024-01-08 23:41:03 -0500

    Update main.c
    Optimization

--- main.c
+++ main.c
@@ -3,33 +3,29 @@
 
 int main(int argc, char *argv[]) {
   char *browser_new_tab = "chromium --new-tab";
+  char *wiki_home_url = "https://terraria.wiki.gg/wiki/Terraria_Wiki";
+  char *wiki_search_url = "https://terraria.wiki.gg/index.php?search=";
   if(argc == 1) { // open the wiki homepage if no arguments are recieved
-    char *open_wiki_home_url = malloc((sizeof(char) * strlen(browser_new_tab)) + 45);
+    char *open_wiki_home_url = malloc((sizeof(char) * strlen(browser_new_tab)) + 1 + strlen(wiki_home_url) + 1);
     strcpy(open_wiki_home_url, browser_new_tab);
-    strcat(open_wiki_home_url, " https://terraria.wiki.gg/wiki/Terraria_Wiki");
+    strcat(open_wiki_home_url, " ");
+    strcat(open_wiki_home_url, wiki_home_url);
     system(open_wiki_home_url);
     free(open_wiki_home_url);
   }
-  else { // if arguments are recieved open the wiki search page using arguments as the search query
-    char *open_wiki_search_url = malloc(sizeof(char) * (strlen(browser_new_tab) + 44));
+  else { // if arguments are recieved concatenate them and search the wiki 
+    int query_length = argc - 1;
+    for(int i = 1; i <= argc - 1; i += 1) query_length += strlen(argv[i]);
+    char *open_wiki_search_url = malloc(sizeof(char) * (strlen(browser_new_tab) + 1 + strlen(wiki_search_url) + query_length + 1));
     strcpy(open_wiki_search_url, browser_new_tab);
-    strcat(open_wiki_search_url, " https://terraria.wiki.gg/index.php?search=");
-    if(argc == 2) {
-      open_wiki_search_url = realloc(open_wiki_search_url, sizeof(char) * (strlen(open_wiki_search_url) + strlen(argv[1]) + 1));
-      strcat(open_wiki_search_url, argv[1]);
-      system(open_wiki_search_url);
-      free(open_wiki_search_url);
+    strcat(open_wiki_search_url, " ");
+    strcat(open_wiki_search_url, wiki_search_url);
+    for(int i = 1; i <= argc - 1; i += 1) {
+      strcat(open_wiki_search_url, argv[i]);
+      if(i != argc - 1) strcat(open_wiki_search_url, "+");
     }
-    else {
-      for(int i = 1; i <= argc - 1; i += 1) {
-        if(i != argc - 1) open_wiki_search_url = realloc(open_wiki_search_url, sizeof(char) * (strlen(open_wiki_search_url) + strlen(argv[i]) + 2));
-        else open_wiki_search_url = realloc(open_wiki_search_url, sizeof(char) * (strlen(open_wiki_search_url) + strlen(argv[i]) + 1));
-        strcat(open_wiki_search_url, argv[i]);
-        if(i != argc - 1) strcat(open_wiki_search_url, "+");
-      }
-      system(open_wiki_search_url);
-      free(open_wiki_search_url);
-    }
+    system(open_wiki_search_url);
+    free(open_wiki_search_url);
   }
   return 0;
 }
